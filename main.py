19e7f2b1e73cb99920b01b8e8cea5981d0dd5554
avashraj <avashraj328@gmail.com>
2024-04-08 16:12:06 -0700

    draft pr

---
+++ main.py
@@ -0,0 +1,28 @@
+'''
+mvp algorithm
+
+cli application first
+
+take in player prop
+check if player data exists in particular team folder
+compare player prop to stats
+if within .5 greater than or less than average, give a good prop
+if not bad prop
+
+need to concatinate player name
+'''
+
+from abc import ABC, abstractmethod
+
+'''
+abstract class that has ratepick method
+
+this method is scaleable
+
+mocked ratepick method to make cli/ future frontend development easier
+
+data handler ?? idk how to organize this becuase all of my data is in csv files
+
+factory class to give out picks to cli??
+'''
+
